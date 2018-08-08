98af7d5f3784424148ba63807b3bdd4c989cd0f2
Sebastian Balaj <darius.balaj@icloud.com>
2018-08-08 20:52:05 +0300

    Home

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	$('.products-menu a').each(function() {
+      $(this).click(function(e) {
+        e.preventDefault();
+          $('.products-menu a').removeClass('active_btn');
+        var link = $(this).attr('rel');
+     $(this).addClass('active_btn');
+     $('.content-products').hide();
+        $('div#' + link).show();
+      });
+});
 	$(window).scroll(function(){
 			if($(window).scrollTop() > 30) {
 				$('.container-top').addClass('scrolled-top');
