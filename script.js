b5c35be94212ba2eb77b9e5363f70b1d7c7b799d
Sebastian Balaj <darius.balaj@icloud.com>
2018-10-28 00:37:11 +0200

    changes

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
 	$('.products-menu a').each(function() {
-      $(this).click(function(e) {
-        e.preventDefault();
-          $('.products-menu a').removeClass('active_btn');
-        var link = $(this).attr('rel');
-     $(this).addClass('active_btn');
-     $('.content-products').hide();
-        $('div#' + link).show();
-      });
-});
-	$(window).scroll(function(){
-			if($(window).scrollTop() > 30) {
-				$('.container-top').addClass('scrolled-top');
-			}
-			else{
-				$('.container-top').removeClass('scrolled-top');
-			}
+		$(this).click(function(e) {
+			e.preventDefault();
+			$('.products-menu a').removeClass('active_btn');
+			var link = $(this).attr('rel');
+			$(this).addClass('active_btn');
+			$('.content-products').hide();
+			$('div#' + link).show();
 		});
+	});
+	if($('.gallery').length > 0){
+	 $('.gallery a').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false,
+      gallery: { enabled:true }
+  })
+	}
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 30) {
+			$('.container-top').addClass('scrolled-top');
+		}
+		else{
+			$('.container-top').removeClass('scrolled-top');
+		}
+	});
 })
